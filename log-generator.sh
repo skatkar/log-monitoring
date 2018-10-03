91d84541d71036c2d8063b0e908112e078582274
skatkar <skatkar225@gmail.com>
2018-10-02 23:41:38 -0700

    Log generator script

--- log-generator.sh
+++ log-generator.sh
@@ -7,22 +7,22 @@ while getopts ":d:i:" opt
 do
   case ${opt} in
     d ) delay=$OPTARG
-		echo "Delay of " $delay " is set"
+        echo "Delay of " $delay " is set"
       ;;
     i ) end=$OPTARG
-		echo "Loop will iterate for " $end " times"
+        echo "Loop will iterate for " $end " times"
       ;;
     \? ) echo "Usage: cmd [-d] [-i]"
       ;;
   esac
 done
 
-
+outputFile=access_log_`date +"%Y%m%d%H%M"`.log
 while [ $start -le $end ]
 do
     while read line
     do
-        echo $line
+        echo $line >> $outputFile
     done<inputSample.txt
     sleep $delay
 ((start++))
